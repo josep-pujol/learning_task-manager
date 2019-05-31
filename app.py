6ab09510e6b7ad9d0657d952992696c6bc92c4dc
Josep <pujol.rabat@gmail.com>
2019-05-31 18:39:57 +0000

    added connection to MongoDB

--- app.py
+++ app.py
@@ -1,8 +1,15 @@
 import os
 from flask import Flask
+from flask_pymongo import PyMongo
+from bson.objectid import ObjectId
 
 app = Flask(__name__)
 
+
+app.config["MONGO_DBNAME"] = 'task_manager'
+app.config["MONGO_URI"] = os.getenv('MONGO_URI')
+
+mongo = PyMongo(app)
 
 @app.route('/')
 def hello():
